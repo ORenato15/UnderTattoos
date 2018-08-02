3d3f353d2fcbfe641d8398ebce13472823a11563
Oscar Canales <oscarenato.15@hotmail.com>
2018-08-02 00:59:19 -0500

    login partial

---
+++ UnderTattoos/UnderTattoos/ViewModels/LoginViewModel.cs
@@ -0,0 +1,123 @@
+﻿namespace UnderTattoos.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Services;
+    using System.Windows.Input;
+    using Views;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Debe ingresar un Email", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Debe ingresar su Password", "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+            }
+
+            var token = await this.apiService.GetToken(
+                "http://tattoosapi.azurewebsites.net",
+                this.Email,
+                this.Password);
+
+            if (token == null)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", "Conexión Perdida", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(token.AccessToken))
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", token.ErrorDescription, "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            var mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token;
+            mainViewModel.Tattoos = new TattoosViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new TattoosPage());
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+        }
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.apiService = new ApiService();
+            this.IsRemembered = true;
+            this.IsEnabled = true;
+        }
+        #endregion
+    }
+}
