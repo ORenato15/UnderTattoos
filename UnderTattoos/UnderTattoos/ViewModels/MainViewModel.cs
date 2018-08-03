8648505566ff84d93c7279947db7df33250aff02
Oscar Canales <oscarenato.15@hotmail.com>
2018-08-03 04:49:04 -0500

    Revert "login partial"
    This reverts commit 3d3f353d2fcbfe641d8398ebce13472823a11563.

--- UnderTattoos/UnderTattoos/ViewModels/MainViewModel.cs
+++ UnderTattoos/UnderTattoos/ViewModels/MainViewModel.cs
@@ -2,19 +2,6 @@
 {
     public class MainViewModel
     {
-        #region ViewModels
-        public LoginViewModel Login
-        {
-            get;
-            set;
-        }
-        #endregion
 
-        #region Constructors
-        public MainViewModel()
-        {
-            this.Login = new LoginViewModel();
-        }
-        #endregion
     }
 }
